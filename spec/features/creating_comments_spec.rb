53c1f50e4bb52298232effa6206ec78aa20efb15
i5okie <ipzent@gmail.com>
2015-02-02 09:06:43 -0800

    When updating a comment's status, also update the ticket's status

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -11,6 +11,8 @@ RSpec.feature "Users can comment on tickets" do
     login_as(user)
     assign_role!(user, :manager, project)
 
+    FactoryGirl.create(:state, name: "Open")
+
     visit "/"
     click_link project.name
   end
@@ -31,5 +33,17 @@ RSpec.feature "Users can comment on tickets" do
     click_button "Create Comment"
 
     expect(page).to have_content("Comment has not been created.")
+  end
+
+  scenario "when changing a ticket's state" do
+    click_link ticket.title
+    fill_in "Text", with: "This is a real issue"
+    select "Open", from: "State"
+    click_button "Create Comment"
+
+    expect(page).to have_content("Comment has been created.")
+    within("#ticket .state") do
+      expect(page).to have_content("Open")
+    end
   end
 end
