a45d89afc45f51241087dd878b2d5b6a448679e0
i5okie <ipzent@gmail.com>
2015-02-02 11:19:23 -0800

    Auto-assign the default state to newly-created tickets

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Creating Tickets" do
+  let!(:state) { FactoryGirl.create :state, name: "New", default: true }
   let(:user) { FactoryGirl.create(:user) }
 
   before do
@@ -19,6 +20,7 @@ RSpec.feature "Creating Tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content("Ticket has been created.")
+    expect(page).to have_content("State: New")
     within("#ticket #author") do
       expect(page).to have_content("Created by #{user.email}")
     end
@@ -48,6 +50,7 @@ RSpec.feature "Creating Tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content("Ticket has been created.")
+    expect(page).to have_content("State: New")
 
     within("#ticket .assets") do
       expect(page).to have_content("speed.txt")
@@ -78,6 +81,7 @@ RSpec.feature "Creating Tickets" do
     click_button "Create Ticket"
 
     expect(page).to have_content("Ticket has been created.")
+    expect(page).to have_content("State: New")
 
     within("#ticket .assets") do
       expect(page).to have_content("speed.txt")
