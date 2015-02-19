9ea11e5d63181cd4e3936ad135bb22b20397a916
i5okie <ipzent@gmail.com>
2015-02-19 12:44:27 -0800

    Users may now search for tickets by state or tag(yes, but test still fails)

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,26 +3,40 @@ require "rails_helper"
 feature "Users can search for tickets matching specific criteria" do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project) }
+  let(:open) { State.create(name: "Open", default: true) }
+  let(:closed) { State.create(name: "Closed") }
+
   let!(:ticket_1) do
     FactoryGirl.create(:ticket, title: "Create projects",
-            project: project, author: user, tag_names: "iteration_1")
+            project: project, author: user, tag_names: "iteration_1",
+            state: open)
   end
 
   let!(:ticket_2) do
     FactoryGirl.create(:ticket, title: "Create users",
-            project: project, author: user, tag_names: "iteration_2")
+            project: project, author: user, tag_names: "iteration_2",
+            state: closed)
   end
 
   before do
-    assign_role!(user, :manager, project)
     login_as(user)
-
+    assign_role!(user, :manager, project)
+    
     visit "/"
     click_link project.name
   end
 
-  scenario "searching by tag" do
+  scenario "searching by tag", js: true do
     fill_in "Search", with: "tag:iteration_1"
+    click_button "Search"
+    within("#tickets") do
+      expect(page).to have_content("Create projects")
+      expect(page).to_not have_content("Create users")
+    end
+  end
+
+  scenario "searching by state", js: true do
+    fill_in "Search", with: "state:Open"
     click_button "Search"
     within("#tickets") do
       expect(page).to have_content("Create projects")
